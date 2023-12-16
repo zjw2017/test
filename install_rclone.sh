d23d8bd637adcd4b4c29efb641660bd7fe5c6266
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-12-17 01:29:40 +0800

    更新 install_rclone.sh

--- install_rclone.sh
+++ install_rclone.sh
@@ -14,7 +14,7 @@ set -e
 
 #set Gitub_Token
 Gitub_Token=$GITHUB_TOKEN
-
+echo $Gitub_Token
 #when adding a tool to the list make sure to also add its corresponding command further in the script
 unzip_tools_list=('unzip' '7z' 'busybox')
 
